e0d46aaab74b701e8719b86f33ca6327290b091c
evanturner2018 <47796999+evanturner2018@users.noreply.github.com>
2019-03-26 01:37:46 -0400

    Add files via upload

---
+++ tt-reasonable-bot/bot.js
@@ -0,0 +1,369 @@
+var HTTPS = require('https');
+var fs = require('fs');
+
+var botID = process.env.BOT_ID;
+
+// Regex (should not change often)
+var helpRegex = /^!help$/;
+var activesRegex = /^!actives$/;
+var accountsRegex = /^!accounts$/;
+var passEmptyRegex = /^!passwords$/;
+var passwordRegex = /^!password \w*$/;
+var calendarRegex = /^!calendar$/;
+var studyRegex = /^!study$/;
+var usernameRegex = /^!username$/;
+var reasonableRegex = /^!reasonable$/;
+var cleanupRegex = /^!cleanup$/;
+var pledgesRegex = /^!pledges$/;
+var doorRegex = /^!door$/;
+var sigmaRegex = /^!sigma \w*$/;
+var capitalizedsigmaRegex = /^!Sigma \w*$/;
+var standardsRegex = /^!standards$/;
+var thanksRegex = /^!thanks$/;
+var nukeRegex = /^!nuke$/;
+var moonRegex = /^!moon$/;
+var barRegex = /^!bar$/;
+var budgetRegex = /^!budget$/;
+
+
+// Personal variables
+var passwords = {};
+var accounts = [];
+
+var actives = {};
+var sigmaNumbers = [];
+
+/* System Functions */
+function readFileToString(filepath, func) {
+  return fs.readFileSync(filepath).toString();
+};
+
+function importAccounts() {
+  // Accounts and passwords
+  var lines = readFileToString('./resources/passwords.txt').split('\n');
+  console.log(lines);
+  for (var i = 0; i < lines.length; i++) {
+    temp = lines[i].split(': ');
+    accounts.push( temp[0] );
+    passwords[temp[0]] = temp[1];
+  };
+  console.log(accounts);
+};
+
+function importActives() {
+    // Actives
+    var lines = readFileToString('./resources/allactives.txt').split('\n');
+    console.log(lines);
+    for (var i = 0; i < lines.length; i++) {
+        temp = lines[i].split(' - ');
+        sigmaNumbers.push( temp[0] );
+        actives[temp[0]] = temp[1];
+    };
+    console.log(sigmaNumbers);
+};
+
+/* Callback fuctions */
+function getActives() {
+    return readFileToString('./resources/actives.txt');
+};
+
+function getPledges() {
+    return readFileToString('./resources/pledges.txt');
+};
+
+function getPassword(account) {
+  if( Object.keys(passwords).length === 0 && JSON.stringify(passwords) === JSON.stringify({})
+    ){ importAccounts() };
+  return passwords[account.toLowerCase()];
+};
+
+function getAccounts() {
+  if(accounts.length == 0){ importAccounts() };
+  return accounts.join('\n')
+};
+
+function getHelpText() {
+  return readFileToString('./resources/help_text.txt');
+};
+
+function getCalendarText() {
+    return readFileToString('./resources/calendar.txt');
+};
+
+function getStudyImage() {
+    return 'http://i.imgur.com/NL43LG2.jpg';
+};
+
+function getUsername(){
+    return 'sigmachapterofthetatau@gmail.com';
+};
+
+function getBar(){
+    return 'https://imgur.com/a/ko7mXc0';
+};
+
+function getReasonable(){
+    return readFileToString('./resources/reasonable_list.txt');
+};
+
+function getCleanup(){
+    return "https://i.imgur.com/Oc7y2FW.png";
+};
+
+function getDoor() {
+    return '3974';
+};
+
+function getStandardsForm() {
+    return 'https://docs.google.com/forms/d/1Nr7-pbG5DrM_DFKNCN0bMHQLuvI56s_nohUFxFzDQSQ/viewform?edit_requested=true';
+};
+
+function getThanks() {
+	var x = Math.floor(Math.random() % 4);
+	var s;
+	switch (x) {
+		case 1:
+			s = '';
+			break;
+		case 2:
+			s = 'No problem';
+			break;
+		case 3:
+			s = 'You are welcome';
+			break;
+		case 4:
+			s = 'You\'re welcome';
+	}
+    return s;
+};
+
+function getSigma(sigmaNumber) {
+    if(Object.keys(actives).length === 0 && JSON.stringify(actives) === JSON.stringify({})
+       ){ importActives() };
+    return actives[sigmaNumber];
+};
+
+function getNuke(){
+    return 'https://i.imgur.com/wL3Uo20.jpg';
+};
+
+function getNukeCountdown(){
+    return 'https://i.imgur.com/dVF4hGD.jpg';
+};
+
+function getDrop(){
+    return 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'; // rick roll
+	 //return '!nuke';
+};
+
+function getMoon(){
+    return 'Do your fucking job @Andrew Campbell';
+};
+
+function getBudget(){
+    return '$0.69';
+};
+
+// Determine what kind of response to make
+function respond() {
+  // Now we do the cool stuff!!
+  var request = JSON.parse(this.req.chunks[0]);
+    var user = JSON.parse;
+
+  // Hey, something happened!
+  if (request.text) {
+    // HELP OUT USER
+    if (helpRegex.test(request.text)) {
+      console.log("!help");
+      this.res.writeHead(200);
+      postMessage(getHelpText);
+      this.res.end();
+    }
+    // GET ACCOUNTS
+    else if (accountsRegex.test(request.text) || passEmptyRegex.test(request.text)) {
+      console.log("!accounts");
+      this.res.writeHead(200);
+      postMessage(getAccounts);
+      this.res.end();
+    }
+    // GET PASSWORD
+    else if (passwordRegex.test(request.text)) {
+      account = request.text.slice(10); //!password
+      console.log("!password " + account);
+      this.res.writeHead(200);
+      postMessage(getPassword, account);
+      this.res.end();
+    }
+    // GET ACTIVES
+    else if (activesRegex.test(request.text)) {
+      console.log("!actives");
+      this.res.writeHead(200);
+      postMessage(getActives);
+      this.res.end();
+    }
+      
+      // GET SPECIFIC ACTIVE
+    else if (sigmaRegex.test(request.text) || capitalizedsigmaRegex.test(request.text)) {
+        sigmaNumber = request.text.slice(7); //!sigma
+        console.log("!sigma " + sigmaNumber);
+        this.res.writeHead(200);
+        postMessage(getSigma, sigmaNumber);
+        this.res.end();
+    }
+      // GET PLEDGES
+    else if (pledgesRegex.test(request.text)) {
+        console.log("!pledges");
+        this.res.writeHead(200);
+        postMessage(getPledges);
+        this.res.end();
+    }
+      // GET CALENDAR
+    else if (calendarRegex.test(request.text)) {
+        console.log("!calendar");
+        this.res.writeHead(200);
+        postMessage(getCalendarText);
+        this.res.end();
+    }
+      
+      // GET STUDY
+    else if (studyRegex.test(request.text)) {
+        console.log("!study");
+        this.res.writeHead(200);
+        postMessage(getStudyImage);
+        this.res.end();
+    }
+      // GET USERNAME
+    else if (usernameRegex.test(request.text)) {
+        console.log("!username");
+        this.res.writeHead(200);
+        postMessage(getUsername);
+        this.res.end();
+    }
+      // GET REASONABLE
+    else if (reasonableRegex.test(request.text)) {
+        console.log("!reasonable");
+        this.res.writeHead(200);
+        postMessage(getReasonable);
+        this.res.end();
+    }
+      
+      // GET CLEANUP
+    else if (cleanupRegex.test(request.text)) {
+        console.log("!cleanup");
+        this.res.writeHead(200);
+        postMessage(getCleanup);
+        this.res.end();
+    }
+
+      // GET BAR
+    else if (barRegex.test(request.text)) {
+        console.log("!bar");
+        this.res.writeHead(200);
+        postMessage(getBar);
+        this.res.end();
+    }
+
+      // GET DOOR
+    else if (doorRegex.test(request.text)) {
+        console.log("!door");
+        this.res.writeHead(200);
+        postMessage(getDoor);
+        this.res.end();
+    }
+
+      // GET STANDARDS FORM
+    else if (standardsRegex.test(request.text)) {
+        console.log("!standards");
+        this.res.writeHead(200);
+        postMessage(getStandardsForm);
+        this.res.end();
+    }
+
+      // GET THANKS
+    else if (thanksRegex.test(request.text)) {
+        console.log("!thanks");
+        this.res.writeHead(200);
+        postMessage(getThanks);
+        this.res.end();
+    }
+
+    // GET MOON
+    else if (moonRegex.test(request.text)) {
+        console.log("!moon");
+        this.res.writeHead(200);
+        postMessage(getMoon);
+        this.res.end();
+    }
+
+    // GET BUDGET
+    else if (budgetRegex.test(request.text)) {
+        console.log("!budget");
+        this.res.writeHead(200);
+        postMessage(getBudget);
+        this.res.end();
+    }
+
+      // GET NUKE (to be used to spam group chat if it all goes hell)
+    else if (nukeRegex.test(request.txt)){
+	console.log("!nuke");
+	//this.res.writeHead(200); // uncomment this to purge
+	//postMessage(getNuke);
+	//this.res.writeHead(200);
+	//postMessage(getNukeCountdown);
+	this.res.writeHead(200);
+        postMessage(getDrop);
+	this.res.end();
+    }
+      
+    // NOT IMPORTANT...
+    else {
+      console.log("don't care");
+      this.res.writeHead(200);
+      this.res.end();
+    }
+  }
+  // Boring......
+  else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+};
+
+// Send the message to the group
+function postMessage(messageFunction, optString) {
+  var botResponse, options, body, botReq;
+
+  botResponse = messageFunction(optString);
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+};
+
+exports.respond = respond;
---
+++ tt-reasonable-bot/resources/fekbot.js
@@ -0,0 +1,172 @@
+var HTTPS = require('https');
+var cool = require('cool-ascii-faces');
+
+var botID = process.env.BOT_ID;
+
+function respond()
+{
+    var request = JSON.parse(this.req.chunks[0]),
+    botRegex = /^\/ski/; botRegexDL = /^\/Cleveland/i;botRegexSalt = /^\/jake/;botRegexRules = /^\/fuf/
+    botRegexAd=/^\/credit card rewards/;botRegexGTA = /^\/H/; botRegexSC = /^\/sup jake/i; botODB = /\/Yo/i; botDuck = /^\/duck/;
+    botRegexP = /^\/TV/i; botRegexTw = /^\/Jake/i; botRegexSb = /^\/Northbrook/; botRegexSh = /^\/wolf/; botRegexWk = /^\/A/; botRegexCC = /^\/snail/;
+    botRegexSiege = /^\/♋️/;
+    siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
+    siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
+    var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
+                  ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
+                  "MIA","BUF","SF","WAS","NYJ","TB"]
+    if(request.text && botRegex.test(request.text))
+    {
+        this.res.writeHead(200);
+        postMessage("http://i.imgur.com/4Ae1GPz.jpg");
+        postMessage("There was a bear so joe only did 3 lines");
+        this.res.end();
+    }
+    else if(request.text && botRegexDL.test(request.text))
+    {
+        this.res.writeHead(200);
+        postMessage("Honestly, ive been putting up a front its a terrible place and we are all trying to move");
+        this.res.end();
+    }
+    else if(request.text && botRegexSalt.test(request.text))
+    {
+        this.res.writeHead(200);
+        postMessage("https://media.giphy.com/media/5xaOcLxIG8VahUOdOpO/giphy.gif");
+        this.res.end();
+    }
+    else if(request.text && botRegexAd.test(request.text))
+    {
+        this.res.writeHead(200);
+        postMessage("");
+        this.res.end();
+    }
+    else if(request.text && botRegexRules.test(request.text))
+    {
+        this.res.writeHead(200);
+        postMessage("http://i.imgur.com/Ci2s66m.jpg");
+        postMessage("show some respect, her names Ellen she is the one that got away");
+        this.res.end();
+    }
+    else if(request.text && botRegexGTA.test(request.text))
+    {
+        this.res.writeHead(200);
+        
+        postMessage("No im not fine i got us bolth fucked up where he went home at 12 and i blacked out but vaguely remember puking at midway");
+        
+        this.res.end();
+    }
+    else if(request.text && botRegexSC.test(request.text))
+    {
+        this.res.writeHead(200);
+        postMessage("pretty thirsty");
+        this.res.end();
+    }
+    else if(request.text && botRegexP.test(request.text)) {
+        this.res.writeHead(200);
+        postMessage("https://media.giphy.com/media/3oJpyi7wBpo0sV9V9C/giphy.gif");
+        postMessage("It was a bird and she wasn't that diabetic");
+        this.res.end();
+    }
+    
+    else if(request.text && botRegexTw.test(request.text))
+    {
+        this.res.writeHead(200);
+        postMessage("ill need a ride my car was stolen");
+        this.res.end();
+    }
+    else if(request.text && botRegexSb.test(request.text)) {
+        this.res.writeHead(200);
+        postMessage("It's sick, ask Sean.");
+        this.res.end();
+    }
+    else if(request.text && botRegexSh.test(request.text))
+    {
+        this.res.writeHead(200);
+        postMessage("http://i.imgur.com/4Ae1GPz.jpg");
+        this.res.end();
+    }
+    else if(request.text && botRegexWk.test(request.text))
+    {
+        this.res.writeHead(200);
+        postMessage("https://i.imgur.com/zYYSYwK.gif");
+        this.res.end();
+    }
+    else if(request.text && botODB.test(request.text))
+    {
+        this.res.writeHead(200);
+        postMessage("THAT IS NOT WHAT HAPPENED");
+        this.res.end();
+    }
+    else if(request.text && botDuck.test(request.text))
+    {
+        this.res.writeHead(200);
+        postMessage("http://media3.giphy.com/media/YCseTHF2I6CCA/giphy.gif");
+        this.res.end();
+    }
+    else if(request.text && botRegexCC.test(request.text))
+    {
+        this.res.writeHead(200);
+        postMessage("since she had mono there was no kissing, so we had unprotected sex");
+        this.res.end();
+    }
+    else if(request.text && botRegexSiege.test(request.text))
+    {
+        this.res.writeHead(200);
+        postMessage("http://i.imgur.com/yXh1wEo.jpg");
+        this.res.end();
+    }
+    
+    else
+    {
+        this.res.writeHead(200);
+        this.res.end();
+    }
+}
+
+function postMessage(response)
+{
+    var botResponse,options, body, botReq;
+    
+    botResponse = response
+    
+    options =
+    {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+    };
+    
+    body =
+    {
+        "bot_id" : botID,
+        "text" : botResponse
+    };
+    
+    console.log('sending ' + botResponse + ' to ' + botID);
+    
+    botReq = HTTPS.request(options, function(res)
+                           {
+                           if(res.statusCode == 202) {
+                           //neat
+                           } else {
+                           console.log('rejecting bad status code ' + res.statusCode);
+                           }
+                           });
+    
+    botReq.on('error', function(err)
+              {
+              console.log('error posting message ' + JSON.stringify(err));
+              });
+    botReq.on('timeout', function(err) {
+              console.log('timeout posting message ' + JSON.stringify(err));
+              });
+    botReq.end(JSON.stringify(body));
+}
+
+function getRandomInt(min, max)
+{
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+exports.respond = respond;
